2c3151ec85157512069f3008465ae6a446c787bf
DeividCezar <perspicilum@gmail.com>
2025-02-02 18:20:34 -0300

    Resize cpu e memory

--- infra/main.tf
+++ infra/main.tf
@@ -7,8 +7,8 @@ resource "aws_ecs_task_definition" "api_task" {
   family                   = "api-processamento-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "2048"
+  memory                   = "4096"
 
   container_definitions = jsonencode([
     {
